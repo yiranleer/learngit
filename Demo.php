a569f42e15474b49a7d182ac44847e4493f68e50
yiranleer <765880041@qq.com>
2016-07-22 13:47:29 +0800

    redis

--- Demo.php
+++ Demo.php
@@ -8,10 +8,26 @@
 
 require_once('./RedisClass.php');
 
+$config = array(
+    'host' => '127.0.0.1',
+    'port' => '6379',
+    'timeout' => '5000'
+);
 
-$redis = new \learngit\RedisClass();
+$redis = new \learngit\RedisClass($config);
 
-$key = 'yiranleer_test';
+$key = 'mySet';
 $value = 'the first redis test!';
-$redis->setStr($key,$value);
+$redis->select(1);
+var_dump($redis->getSetMembers($key));
+
+$key_one = 'set1';
+$key_two = 'set2';
+$redis->setSet($key_two,json_encode(array('John','Tom','Tonny')));
+
+$redis->setSet($key_one,json_encode(array('John','Tonny')));
+
+var_dump($redis->getIntersection($key_one,$key_two));
+
+$redis->setDelete($key_one,json_encode(array('John','Tonny')));
 
--- RedisClass.php
+++ RedisClass.php
@@ -19,9 +19,9 @@ class RedisClass {
      */
     public function __construct($config=array()){
 
-        if($config['host'] == '')    $config['host'] = '127.0.0.1';
-        if($config['port'] == '')    $config['port'] = '6379';
-        if($config['timeout'] == '') $config['timeout'] = '5000';
+//        if($config['host'] == '')    $config['host'] = '127.0.0.1';
+//        if($config['port'] == '')    $config['port'] = '6379';
+//        if($config['timeout'] == '') $config['timeout'] = '5000';
         $this->redis = new \Redis();
         $this->redis->connect($config['host'],$config['port'],$config['timeout'] );
 
@@ -30,6 +30,11 @@ class RedisClass {
         }
 
         return $this->redis;
+    }
+
+    public function select($id){
+
+        $this->redis->select($id);
     }
 
     /**
@@ -46,9 +51,78 @@ class RedisClass {
         return $result;
     }
 
+    /**
+     * get str
+     * @param $key
+     * @return mixed
+     *
+     */
     public function getStr($key){
 
         $result = $this->redis->get($key);
         return json_decode($result,true);
+    }
+
+    /**
+     * delete one str
+     * @param $key
+     */
+    public function deleteStr($key){
+
+        return $this->redis->delete($key);
+    }
+
+    /**
+     * add unsorted set
+     * @param $key
+     * @param $value
+     * @return int
+     */
+    public function setSet($key,$value){
+
+        $result = $this->redis->sAdd($key,$value);
+        return $result;
+    }
+
+    /**
+     * get members of a set
+     * @param $key
+     * @return array
+     */
+    public function getSetMembers($key){
+
+        return $this->redis->sMembers($key);
+    }
+
+    /**
+     * get intersection of two sets
+     * @param $key_one
+     * @param $key_two
+     */
+    public function getIntersection($key_one,$key_two){
+
+        return $this->redis->sInter($key_one,$key_two);
+    }
+
+    /**
+     * remove one str from the set
+     * @param $key
+     * @param $str: which you want to remove
+     */
+    public function setDelete($key,$str){
+
+        return $this->redis->sRemove($key,$str);
+    }
+
+    /**
+     * move str from key_one to key_two
+     * @param $key_one
+     * @param $key_two
+     * @param $str
+     * @return bool
+     */
+    public function sMove($key_one,$key_two,$str){
+
+        return $this->redis->sMove($key_one,$key_two,$str);
     }
 } 
